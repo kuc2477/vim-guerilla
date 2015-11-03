aea84c292c4556e5425d47fa215d080a5d69d6ca
gsin <gsin@ssangko.com>
2015-11-03 13:42:03 +0900

    implement infiltrate.sh

--- infiltrate.bat
+++ infiltrate.bat
@@ -1,1 +1,10 @@
+git submodule update --init
 
+:: create backup of system's current vim configurationsreate
+mkdir _bak
+mkdir _bak\_vim
+xcopy %USERPROFILE%\_vimrc _bak
+xcopy %USERPROFILE%\_vim _bak\_vim
+
+
+:: deploy `vim-guerilla` to the system
--- infiltrate.sh
+++ infiltrate.sh
@@ -1,12 +1,12 @@
-  git submodule update --init
-  
-  # create backup of system's current vim configurations
-  mkdir .bak
-  cp $HOME/.vimrc `pwd`/`dirname $0`/.bak/vimrc.bak
-  cp $HOME/.vim `pwd`/`dirname $0`/.bak/vim.bak
-  
-  # deploy `vim-guerilla` to the system
-  ln -sfi `pwd`/`dirname $0`/vimrc $HOME/.vimrc
-  ln -sfi `pwd`/`dirname $0`/vim $HOME/.vim
-  vim +PluginInstall +qall
-  vim +PluginClean! +qall
+git submodule update --init
+
+# create backup of system's current vim configurations
+mkdir .bak
+cp $HOME/.vimrc `pwd`/`dirname $0`/.bak/vimrc.bak
+cp $HOME/.vim `pwd`/`dirname $0`/.bak/vim.bak
+
+# deploy `vim-guerilla` to the system
+ln -sfi `pwd`/`dirname $0`/vimrc $HOME/.vimrc
+ln -sfi `pwd`/`dirname $0`/vim $HOME/.vim
+vim +PluginInstall +qall
+vim +PluginClean! +qall
