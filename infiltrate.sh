0cc864e40c7b0a0252acda05d3a080e1bab96930
June <kuc2477@gmail.com>
2015-11-04 12:55:05 +0900

    linux script ready

--- infiltrate.sh
+++ infiltrate.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
 
+# =====
+# Paths
+# =====
+
+# shelter
+SHELTER="$(pwd)/$(dirname $0)/.shelter"
+
+# vim
+GUERILLA_VIMRC="$(pwd)/$(dirname $0)/vimrc"
+GUERILLA_VIMDIR="$(pwd)/$(dirname $0)/vim"
+SYSTEM_VIMRC="$HOME/.vimrc"
+SYSTEM_VIMDIR="$HOME/.vim"
+
+
+# ============
+# Infiltration 
+# ============
+
 git submodule update --init
 
-# vimrcs
-src_vimrc="`pwd`/`dirname $0`/vimrc"
-dest_vimrc="$HOME/.vimrc"
+if [ ! -f "$SYSTEM_VIMRC" && \
+  "$(cat "$GUERILLA_VIMRC")" != \
+  "$(cat "$SYSTEM_VIMRC")"]; then
+  # create shelter if it doesn't exists
+  if [ ! -d "$SHELTER" ]; then mkdir "$SHELTER"; fi
 
-if [[ ! -f "$HOME/.vimrc" || "`cat "$src_vimrc"`" != "`cat "$dest_vimrc"`"]]; then
-  # create backup of system's current vim configurations
-  mkdir .bak
-  mv $HOME/.vimrc `pwd`/`dirname $0`/.bak/vimrc.bak
-  mv $HOME/.vim `pwd`/`dirname $0`/.bak/vim.bak
+  # evacuate system vimrc
+  mv "$SYSTEM_VIMRC" "$SHELTER/vimrc"
 
-  # deploy `vim-guerilla` to the system
-  ln -sfi `pwd`/`dirname $0`/vimrc $HOME/.vimrc
-  ln -sfi `pwd`/`dirname $0`/vim $HOME/.vim
+  # evacuate system vimdir if exists
+  if [ -d "$SYSTEM_VIMDIR" ]; then 
+    mv "$SYSTEM_VIMDIR" "$SHELTER/vim"; 
+  fi
+
+  # deploy guerilla to the system
+  cp -r "$GUERILLA_VIMRC" "$HOME/.vimrc"
+  cp -r "$GUERILLA_VIMDIR" "$HOME/.vim"
 fi
 
 # install plugins
--- retreat.sh
+++ retreat.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+
+# =====
+# Paths
+# =====
+
+# shelter
+SHELTER="$(pwd)/$(dirname $0)/.shelter"
+
+# vim
+SYSTEM_VIMRC="$SHELTER/.vimrc"
+SYSTEM_VIMDIR="$SHELTER/.vim"
+
+
+# =======
+# Retreat
+# =======
+
+if [ ! -f "$SYSTEM_VIMRC" ]; then
+  # restore system vimrc if exists in the shelter
+  mv "$SYSTEM_VIMRC" "$HOME/.vimrc"
+
+  # restore system vimdir if exists in the shelter
+  if [ -d "$SYSTEM_VIMDIR" ]; then
+    mv "$SYSTEM_VIMDIR" "$HOME/.vim"
+  fi
+fi
